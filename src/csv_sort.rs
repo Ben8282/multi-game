da5738e2b6dfd3ecd9af47544e98d1f6a7b9eca6
Ben8282 <benchorev@gmail.com>
2026-08-27 18:07:07 +0700

    Refactor CSV file input handling to improve flexibility and remove redundant code 😃😃😃😃😃😃

--- src/csv_sort.rs
+++ src/csv_sort.rs
@@ -19,7 +19,6 @@ pub fn sort_csv() {
         }
     };
     if choice == "1" {
-<<<<<<< HEAD
         let (file, file_path) = loop {
             println!("please enter the csv file path");
             let mut input = String::new();
@@ -33,15 +32,6 @@ pub fn sort_csv() {
         let mut reader = csv::ReaderBuilder::new()
             .has_headers(false)
             .flexible(true) // rows with differing field counts are not an error
-=======
-        println!("please enter the csv file path");
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        let file_path = input.trim().to_string();
-        let file = File::open(&file_path).unwrap();
-        let mut reader = csv::ReaderBuilder::new()
-            .has_headers(false)
->>>>>>> b1a7540fc5b71636670a79cb887e74a352c17df3
             //.delimiter(b',') optional, default is b','
             .from_reader(file);
         let mut records: Vec<BigDecimal> = Vec::new();
@@ -107,7 +97,6 @@ pub fn sort_csv() {
             }
         }
     } else if choice == "2" {
-<<<<<<< HEAD
         let (file, file_path) = loop {
             println!("please enter the csv file path");
             let mut input = String::new();
@@ -121,15 +110,6 @@ pub fn sort_csv() {
         let mut reader = csv::ReaderBuilder::new()
             .has_headers(false)
             .flexible(true) // rows with differing field counts are not an error
-=======
-        println!("please enter the csv file path");
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        let file_path = input.trim().to_string();
-        let file = File::open(&file_path).unwrap();
-        let mut reader = csv::ReaderBuilder::new()
-            .has_headers(false)
->>>>>>> b1a7540fc5b71636670a79cb887e74a352c17df3
             //.delimiter(b',') optional, default is b','
             .from_reader(file);
         let mut records: Vec<BigDecimal> = Vec::new();
